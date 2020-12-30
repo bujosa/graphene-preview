da09347327da6243575ff8576a009e8cccce209d
bujosa <davidbujosa@gmail.com>
2020-12-30 02:47:34 -0400

    Finished set first schema query with graphene

--- schema.py
+++ schema.py
@@ -1,8 +1,33 @@
 import graphene
 import json 
-
+from datetime import datetime
 class User(graphene.ObjectType):
     id = graphene.ID()
     username = graphene.String()
-    last_login = graphene.Date()
+    lastLogin = graphene.Date()
 
+class Query(graphene.ObjectType):
+    users = graphene.List(User, first=graphene.Int())
+
+    def resolve_users(self, info, first):
+        return [
+            User(username='Test1', lastLogin=datetime.now()),
+            User(username='Test2', lastLogin=datetime.now()),
+            User(username='Test3', lastLogin=datetime.now())
+        ][:first]
+
+schema = graphene.Schema(query=Query)
+
+result = schema.execute(
+    '''
+     {
+         users(first: 2){
+             id
+             username
+             lastLogin
+         }
+     }
+    '''
+)
+
+print(result)
